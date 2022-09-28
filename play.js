022456dc35f846a5333c38382b08c4896c57799d
zachmanac <zachstephenson94@gmail.com>
2022-09-28 20:22:25 +0000

    added play.js

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: "50541" // PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("Connected to server");
+  });
+  
+  conn.on("data", (data) => {
+    console.log("Server says ", data)
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
